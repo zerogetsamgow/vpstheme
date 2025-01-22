5cce9877ea7c45dd5d934fd537e5cbef97ea285e
Sam Gow (Health) <sam.gow@health.vic.gov.au>
2025-01-22 14:59:06 +1100

    Other changes

--- tests/testthat/test-theme_vps_dh.R
+++ tests/testthat/test-theme_vps_dh.R
@@ -1,18 +1,18 @@
 
 # Create test plot
 test_p =
-  ggplot(
+  ggplot2::ggplot(
     mtcars,
-    aes(hp, wt)) +
-  geom_point(
-    aes(colour = factor(gear)),
+    ggplot2::aes(hp, wt)) +
+  ggplot2::geom_point(
+    ggplot2::aes(colour = factor(gear)),
         size = 6) +
-  labs(
+  ggplot2::labs(
     title = "This is the title of the plot",
     y = "y-axis title",
     x = "x-axis title")
 
 # Use plot in test
 test_that("theme_vps_dh() works", {
-  vdiffr::expect_doppelganger("geom-col-rounded", test_p + theme_vps_dh())
+  vdiffr::expect_doppelganger("theme_vps_dh", test_p + theme_vps_dh())
 })
